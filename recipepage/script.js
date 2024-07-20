14d430298bfb446e239fa60162950f6c9323d761
ChasVanDav <blackkoreangirl@gmail.com>
2024-07-19 23:48:39 -0400

    adding a check box but still need things worked out

--- recipepage/script.js
+++ recipepage/script.js
@@ -6,3 +6,23 @@ newListItem.textContent = 'Jalapenos';
 
 // And finally, let's add that list item as a child of the ul.
 document.querySelector('ul').appendChild(newListItem);
+
+//Adding checkboxes
+const ingredientList = document.getElementById("ingredientList");
+let checkbox = document.createElement("input");
+
+checkbox.type = "checkbox";
+checkbox.name = "name";
+checkbox.value = "value";
+checkbox.id = "id";
+
+let label = document.createElement("label");
+
+label.htmlFor = "id";
+
+label.appendChild(
+    document.createTextNode("This creates the label for checkbox")
+);
+
+ingredientList.appendChild(checkbox);
+ingredientList.appendChild(label);
