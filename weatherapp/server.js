4ac52b21bed093e6ec06fbbfb162471de3da5e1c
ChasVanDav <blackkoreangirl@gmail.com>
2024-08-29 09:43:06 -0400

    api token, fetch request, homepage message updated

--- weatherapp/server.js
+++ weatherapp/server.js
@@ -2,10 +2,26 @@ import express from 'express'
 const app = express()
 const port = 3000
 
+const apiToken = '2a4b9240693bc44d971fbf9becc24fef';
+let cityName = '';
+
+fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${apiToken}`)
+  .then(response => response.json())
+  .then(data => console.log(data))
+  .catch(error => console.error(error));
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('The Vanessa Weather App Homepage!')
 })
 
+app.get('/weather', (req, res) => {
+  req.query.cityName;
+});
+
+
+
+
+
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Listening on port ${port}`)
 })
