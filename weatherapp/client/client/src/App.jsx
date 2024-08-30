a6ad6d653b3c55eb4704ff642ade31018e5ec54a
ChasVanDav <blackkoreangirl@gmail.com>
2024-08-30 14:56:32 -0400

    removing default vite code

--- weatherapp/client/client/src/App.jsx
+++ weatherapp/client/client/src/App.jsx
@@ -1,13 +1,10 @@
 import { useState } from 'react';
 import './App.css';
 
-function App() {
+export default function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div>The Vanessa Weather App Frontend Under Construction</div>
-    //
-  )
-}
-
-export default App;
+  );
+};
--- weatherapp/server.js
+++ weatherapp/server.js
@@ -2,10 +2,10 @@ import express from 'express'
 const app = express()
 const port = 3000
 
-// const apiToken = 'a798fd84a2c6e6b9e00c84561c872b9c'; 
+const apiToken = 'a798fd84a2c6e6b9e00c84561c872b9c'; 
 const cityName = 'New York';
 
-const apiToken = process.env.apikey
+// const apiToken = process.env.apikey
 
 app.get('/', (req, res) => {
   fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiToken}&units=imperial`)
@@ -13,10 +13,6 @@ app.get('/', (req, res) => {
   .then(data => res.send(data))
   .catch(error => res.send(error));
 })
-
-// app.get('/:', (req, res) => {
-//   req.query.cityName;
-// });
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`)
