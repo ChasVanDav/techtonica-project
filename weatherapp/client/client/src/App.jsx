73767091c3edf5d0b7fb593a3d491e88400ac698
ChasVanDav <blackkoreangirl@gmail.com>
2024-08-30 15:12:06 -0400

    making additional changes from default Vite code

--- weatherapp/client/client/src/App.jsx
+++ weatherapp/client/client/src/App.jsx
@@ -2,7 +2,6 @@ import { useState } from 'react';
 import './App.css';
 
 export default function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div>The Vanessa Weather App Frontend Under Construction</div>
