71a44b8d922f35e1dc52fbbea322dcc7ea0eaa64
ChasVanDav <blackkoreangirl@gmail.com>
2024-08-30 15:11:33 -0400

    added query language to cityName variable

--- weatherapp/server.js
+++ weatherapp/server.js
@@ -1,9 +1,11 @@
-import express from 'express'
+import express from 'express';
+import fetch from 'node-fetch';
+
 const app = express()
 const port = 3000
 
 const apiToken = 'a798fd84a2c6e6b9e00c84561c872b9c'; 
-const cityName = 'New York';
+const cityName = req.query.city || 'New York'; //setting the default city
 
 // const apiToken = process.env.apikey
 
@@ -11,7 +13,8 @@ app.get('/', (req, res) => {
   fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiToken}&units=imperial`)
   .then(response => response.json())
   .then(data => res.send(data))
-  .catch(error => res.send(error));
+  //error message for developer and user
+  .catch(error => res.status(500).send({ error:'Oh no! Something went wrong!' }));
 })
 
 app.listen(port, () => {
