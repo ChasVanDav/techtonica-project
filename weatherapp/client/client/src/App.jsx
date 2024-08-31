bee7737bdeb3a51c710b664bca183ce3276e5209
ChasVanDav <blackkoreangirl@gmail.com>
2024-08-31 00:07:28 -0400

    added weather icon

--- weatherapp/client/client/src/App.jsx
+++ weatherapp/client/client/src/App.jsx
@@ -4,15 +4,29 @@ import './App.css';
 
 function App() {
   const [city, setCity] = useState('');
-  const [weather, setWeather] = useState(null);
+  const [weather, setWeather] = useState();
+  const [errorMessage, setErrorMessage] = useState();
 
   const handleSearch = () => {
+    //connected to backend with direct link below
   fetch(`http://localhost:3000?city=${encodeURI(city)}`)
       .then(response => response.json())
-      .then(data => setWeather(data))
-      .catch(error => console.error('Error:', error));
-  };
+      .then(data => {
+        if (data.cod === "404") {
+          throw new Error(data.message)
+        }
 
+        setWeather(data)
+        setErrorMessage()
+
+      })
+      .catch(error => {
+        setErrorMessage(error.message)
+        setWeather()
+      });
+
+  };
+  const errorStyle={color:"red"}
   return (
     <div>
       <h1>Weather App</h1>
@@ -23,11 +37,21 @@ function App() {
         placeholder="Enter city name" 
       />
       <button onClick={handleSearch}>Search</button>
+      {errorMessage && (
+        <div style={errorStyle}>
+          Error: {errorMessage}
+        </div>
+      )}
       {weather && (
         <div>
           <h2>Weather in {weather.name}</h2>
+          {/* math.round */}
           <p>Temperature: {weather.main.temp}°F</p>
+          <p>Humidity: {weather.main.humidity}</p>
+          {/* math.round */}
+          <p>Wind Speed: {weather.wind.speed}</p>
           <p>Condition: {weather.weather[0].description}</p>
+          <img src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} alt={`An icon image of ${weather.weather[0].description}`} />
         </div>
       )}
     </div>
